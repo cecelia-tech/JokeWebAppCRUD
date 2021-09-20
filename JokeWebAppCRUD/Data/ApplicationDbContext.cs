296b19a2782edd737192ebc43749a1ebb29a6d4d
Vita <vitagriciute@gmail.com>
2021-09-20 16:49:57 +0300

    Database migrations, functioning basic app

--- JokeWebAppCRUD/Data/ApplicationDbContext.cs
+++ JokeWebAppCRUD/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using JokeWebAppCRUD.Models;
 
 namespace JokeWebAppCRUD.Data
 {
@@ -12,5 +13,6 @@ namespace JokeWebAppCRUD.Data
             : base(options)
         {
         }
+        public DbSet<JokeWebAppCRUD.Models.Joke> Joke { get; set; }
     }
 }
